def63a913a43599d9c570b298551b21ac43c62ef
lgmarin <11378945+lgmarin@users.noreply.github.com>
2022-07-06 13:49:22 -0300

    Add DHT configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
 #include <Arduino.h>
+#include <DHT.h>
+
+// DHT CONFIGURATION
+#define DHTTYPE     DHT11
+#define DHTPIN      2 //D4
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Screen CONFIGURATION
+
 
 void setup() {
   // put your setup code here, to run once:

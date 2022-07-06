2f84a4753d7a67ebcb4b8ac23c42e1cc233e6ea4
lgmarin <11378945+lgmarin@users.noreply.github.com>
2022-07-06 14:04:47 -0300

    Read DHT data

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,21 @@ DHT dht(DHTPIN, DHTTYPE);
 
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(500);
+
+  dht.begin();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(2500);
+
+  float temperatre = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  if (isnan(humidity) || isnan(temperatre)) {
+    Serial.println(F("[ERROR]: Failed to read from DHT sensor!"));
+  }
+  Serial.print(F("Temp: ")); Serial.print(temperatre); 
+  Serial.print(F("Humid: ")); Serial.print(humidity);
 }

f353bee4d43d3b938fbd9d2991dc8ec1fdf40f89
lgmarin <11378945+lgmarin@users.noreply.github.com>
2022-07-06 14:13:32 -0300

    Read DHT

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
 #include <DHT.h>
+#include <DHT_U.h>
 
 // DHT CONFIGURATION
 #define DHTTYPE     DHT11
 #define DHTPIN      2 //D4
+uint32_t            dhtDelay;
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT_Unified dht(DHTPIN, DHTTYPE);
 
 // Screen CONFIGURATION
 
@@ -14,18 +16,37 @@ void setup() {
   Serial.begin(9600);
   delay(500);
 
+  // Initialize DHT
   dht.begin();
+  sensor_t dhtSensor;
+  dhtDelay = dhtSensor.min_delay / 1000;
+
 
 }
 
 void loop() {
   delay(2500);
 
-  float temperatre = dht.readTemperature();
-  float humidity = dht.readHumidity();
-  if (isnan(humidity) || isnan(temperatre)) {
-    Serial.println(F("[ERROR]: Failed to read from DHT sensor!"));
+  // DHT EVENT HANDLING
+  sensors_event_t event;
+
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
   }
-  Serial.print(F("Temp: ")); Serial.print(temperatre); 
-  Serial.print(F("Humid: ")); Serial.print(humidity);
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
 }

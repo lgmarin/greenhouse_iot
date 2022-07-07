1c792ae9f9fd89274c4e12d51093dae7f9b316f3
lgmarin <11378945+lgmarin@users.noreply.github.com>
2022-07-07 16:02:45 -0300

    Soil Moisture functions

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -16,17 +16,29 @@ SoilMoistureSensor::SoilMoistureSensor(int pin, int airM, int watM)
     calibrated = true;  
 }
 
-int SoilMoistureSensor::readPercent()
-{
-
-}
-
 int SoilMoistureSensor::readValue()
 {
+    return analogRead(analogPin);
+}
 
+int SoilMoistureSensor::readPercent()
+{
+    if (calibrated)
+    {
+        int val = analogRead(analogPin);
+        return map(val, airMoist, waterMoist, 0, 100);
+    }
+    return 00;
 }
 
 void SoilMoistureSensor::calibrate()
 {
-
+    Serial.print(F("\n<<< SOIL MOISTURE CALIBRATION >>>"));
+    Serial.print(F("\n1. Leave the probe in the air (probe dry)."));
+    Serial.print(F("\n--> Take note of this measurement! "));
+    Serial.print(F("\n2. Insert the probe in a glass of water."));
+    Serial.print(F("\n--> Take note of this measurement!"));
+    Serial.print(F("\nREAD THE INSTRUCTIONS AND PRESS ENTER!"));
+    Serial.read();
+    Serial.print(F("\nCURRENT READING: ")); Serial.print(readValue());
 }

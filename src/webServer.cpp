5dd04427a68fb19b4be67f83da5715f49c9c21b7
lgmarin <11378945+lgmarin@users.noreply.github.com>
2022-07-15 11:49:15 -0300

    Refactor notfound

--- src/webServer.cpp
+++ src/webServer.cpp
@@ -5,7 +5,19 @@ AsyncEventSource  events("/events");
 
 // Request Handlers
 void notFound(AsyncWebServerRequest *request) {
-    request->send(404, "text/plain", "Not found");
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += request->url();
+  message += "\nMethod: ";
+  message += (request->method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += request->args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < request->args(); i++) {
+    message += " " + request->argName(i) + ": " + request->arg(i) + "\n";
+  }
+  request->send(404, "text/plain", message);
 }
 
 

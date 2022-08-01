70f7f1d77b64c2aeb8b69f8f787a25940da57abc
lgmarin <11378945+lgmarin@users.noreply.github.com>
2022-08-01 14:18:33 -0300

    Fix script

--- scripts/prepare_fs.py
+++ scripts/prepare_fs.py
@@ -18,21 +18,19 @@ def gzip_webfiles(source, target, env):
     exclude_files = ['svg']
 
     data_src_dir_path = os.path.join(env.get('PROJECT_DIR'), 'web')
-    data_dir_path = env.get('PROJECTDATA_DIR')
+    data_dir_path = os.path.join(env.get('PROJECT_DIR'), 'data')
 
-    print(f'\n[PRE-BUILDING]: Compressing Web files in {data_src_dir_path}...')
+    print(f'[PRE-BUILDING]: Compressing Web files in {data_src_dir_path}...')
 
     # Check if dir exists
     if(os.path.exists(data_dir_path) and not os.path.exists(data_src_dir_path) ):
-        print(f'\n[PRE-BUILDING - ERROR]: Source data dir {data_src_dir_path} not found!')
+        print(f'[PRE-BUILDING - ERROR]: Source data dir {data_src_dir_path} not found!')
+        return
 
     # Clear data dir before compressing
     if(os.path.exists(data_dir_path)):
-        print('\n[PRE-BUILDING]: Clearing {data_dir_path}...')
-        shutil.rmtree(data_dir_path)
-
-    print('\n[PRE-BUILDING]: Creating {data_dir_path}...')
-    os.mkdir(data_dir_path)
+        print(f'[PRE-BUILDING]: Creating data dir: {data_dir_path}')
+        os.mkdir(data_dir_path)
 
     # Prepare a list of all files to compress
     files_to_gzip = []
@@ -43,29 +41,33 @@ def gzip_webfiles(source, target, env):
     files_to_copy = list(set(all_files) - set(files_to_gzip) - set(exclude_files))
 
     for file in files_to_copy:
-        print(f'\n[PRE-BUILDING]: Copying files to {data_dir_path}...')
+        print(f'[PRE-BUILDING]: Copying {file} to {data_dir_path}')
         shutil.copy(file, data_dir_path)
 
     was_error = False
 
     try:
         for source_file_path in files_to_gzip:
-            print(f'\n[PRE-BUILDING]: Compressing files in {source_file_path}...')
-
             base_file_path = source_file_path
             target_file_path = os.path.join(data_dir_path, os.path.basename( base_file_path ) + '.gz')
 
+            if os.path.exists( target_file_path ):
+                print(f'[PRE-BUILDING]: Removing previous version {target_file_path}')
+                os.remove( target_file_path )
+
+            print(f'[PRE-BUILDING]: Compressing file {source_file_path}')
             gzip_file(source_file_path, target_file_path)
 
     except IOError as e:
         was_error = True
-        print(f'\n[PRE-BUILDING - ERROR]: Failure when compressing {source_file_path}! \nError: {e}.')
+        print(f'[PRE-BUILDING - ERROR]: Failure when compressing {source_file_path}! \nError: {e}.')
+        return
 
     if was_error:
-        print(f'\n[PRE-BUILDING - ERROR]: Some files could not be compressed!')
+        print(f'[PRE-BUILDING - ERROR]: Some files could not be compressed!')
 
     else:
-        print(f'\n[PRE-BUILDING]: Files successfully compressed.')
+        print(f'[PRE-BUILDING]: Files successfully compressed.')
 
 # IMPORTANT, this needs to be added to call the routine
 env.AddPreAction('$BUILD_DIR/littlefs.bin', gzip_webfiles)            

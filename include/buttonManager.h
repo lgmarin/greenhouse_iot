6436472a3f79850c11cfd6658559da4acdf755eb
Luiz Marin <11378945+lgmarin@users.noreply.github.com>
2023-01-31 17:15:31 -0300

    Separate and implement OneButton like manager

---
+++ include/buttonManager.h
@@ -0,0 +1,52 @@
+#ifndef btnmanager_h
+#define btnmanager_h
+
+#include <definitions.h>
+
+extern "C" {
+  typedef void (*buttonCallback)(void);
+}
+
+class Button
+{
+private:
+    uint8_t             _pin;
+    unsigned long       _clickDelay = 400;
+    unsigned long       _pressDelay = 800;
+    unsigned long       _debounceDelay = 50;
+    unsigned long       _startTime = 0;
+    int                 _nClicks = 0;
+
+    enum buttonState_t : int {
+        BTN_STATE_INIT      = 0,
+        BTN_STATE_DOWN      = 1,
+        BTN_STATE_UP        = 2,
+        BTN_STATE_COUNT     = 3,
+        BTN_STATE_PRESS     = 6,
+        BTN_STATE_PRESSEND  = 7,
+    };
+
+    buttonState_t _state = BTN_STATE_INIT;
+    buttonState_t _lastState = BTN_STATE_INIT;
+
+public:
+    Button(uint8_t pin) {
+        _pin = pin;
+        pinMode(_pin, INPUT);
+        digitalWrite(_pin, HIGH);
+    }
+    ~Button() {}
+    void ButtonLoop();
+
+private:
+    void _newState(buttonState_t nextState);
+    void _reset();
+    void _checkState(bool isActive);
+
+    //Callbacks
+    buttonCallback _singleClickFunc = NULL;
+    buttonCallback _doubleClickFunc = NULL;
+    buttonCallback _longPressFunc = NULL;
+};
+
+#endif
